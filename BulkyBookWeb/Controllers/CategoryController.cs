1e4fb240664f1b2312b3784cd53384af8a884798
Roshan_KFT <roshan@knowledgeflex.com>
2022-01-21 00:59:02 +0530

    Add project files.

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,119 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+
+        //Get
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly mathch the Name.");
+            }
+            if (ModelState.IsValid) { //it determine model is valid or not
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category Created Sucessfully";
+                return RedirectToAction("Index");   
+            }
+            return View(obj);
+        }
+
+        //Get
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            //var categoryFromDbFirst = _db.Categories.FirstOrDefault(u=>u.ID== id);
+            //var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.ID == id);
+
+            if(categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly mathch the Name.");
+            }
+            if (ModelState.IsValid)
+            { //it determine model is valid or not
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category Updated Sucessfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //Get
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            //var categoryFromDbFirst = _db.Categories.FirstOrDefault(u=>u.ID== id);
+            //var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.ID == id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        //Post
+        [HttpPost, ActionName("Delete")]
+        [HttpPost]// if we use this we need to specified at delete page <form method="post" asp-action="DeleteSingle">
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteSingle(int? id)
+        {
+            var obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category Deleted Sucessfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        //Constructor Of ApplicationDbContext :DbContext
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        //Create Table in Database as categories
+        public DbSet<Category> Categories { get; set; } 
+    }
+}
